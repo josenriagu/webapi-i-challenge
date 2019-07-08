1de5752ffa3b87bf4f9d6415e0ad24c8cd39b68b
Josemaria <josemarianriagu25@gmail.com>
2019-07-08 19:19:13 +0800

    implement GET api initialize server using VSCode debugger

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+   // perfomr the find() operation
+   db.find()
+      .then(users => {
+         // respond with 200 OK and sends the users object
+         res.status(200).json(users);
+      })
+      .catch(err => {
+         res.status(500).json({
+            // if error, responds with 500 and sends an error message
+            error: 'The users information could not be retrieved.',
+            err,
+         });
+      });
+});
+
+server.listen(5000, () => {
+   console.log('\n*** Awesome!! Server Running on http://localhost:5000 ***\n');
+});
