822a43ffd92759d32f83af9c56bd3676b41209cf
Josemaria <josemarianriagu25@gmail.com>
2019-07-08 19:40:48 +0800

    implement GET by ID api

--- index.js
+++ index.js
@@ -5,16 +5,49 @@ const db = require('./data/db.js');
 const server = express();
 server.use(express.json());
 
+// get all users
 server.get('/api/users', (req, res) => {
-   // perfomr the find() operation
+   // perform the find() operation
    db.find()
       .then(users => {
-         // respond with 200 OK and sends the users object
-         res.status(200).json(users);
+         // respond with 200 OK and send the users object
+         res.status(200).json({
+            success: true,
+            users
+         });
+      })
+      .catch(err => {
+         // if error, respond with 500 and send an error message
+         res.status(500).json({
+            error: 'The users information could not be retrieved.',
+            err,
+         });
+      });
+});
+
+// get user by specified ID
+server.get('/api/users/:id', (req, res) => {
+   const id = req.params.id;
+   // perform the findById() operation
+   db.findById(id)
+      .then(user => {
+         // if user exists, respond with 200 OK and send the users object
+         if (user) {
+            res.status(200).json({
+               success: true,
+               user,
+            });
+         } else {
+            // else return a 404 and an error message
+            res.status(404).json({
+               success: false,
+               message: 'The user with the specified ID does not exist.',
+            });
+         }
       })
       .catch(err => {
          res.status(500).json({
-            // if error, responds with 500 and sends an error message
+            // if error, responds with 500 and send an error message
             error: 'The users information could not be retrieved.',
             err,
          });
